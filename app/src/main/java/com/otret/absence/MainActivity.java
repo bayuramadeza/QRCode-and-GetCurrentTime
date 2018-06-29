56f5d33de6d59b63e780ac7870442384d4a65338
Bayu Ramadeza <bayuramadeza@gmail.com>
2018-06-29 17:07:15 +0700

    Add radius to take your absence

--- app/src/main/java/com/otret/absence/MainActivity.java
+++ app/src/main/java/com/otret/absence/MainActivity.java
@@ -11,38 +11,63 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
     private String TAG = "ABSENCE";
     private final int RECORD_REQUEST_CODE = 101;
-    private Button bAbsence;
+
+    @BindView(R.id.b_qr_code) Button bAbsence;
+    @BindView(R.id.iv_map) ImageView ivMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        bAbsence = findViewById(R.id.b_qr_code);
-
-        int permission = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.CAMERA);
-
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            makeRequest();
-        }
-
-        bAbsence.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ReadQR.class);
-                startActivityForResult(intent, 1);
-            }
-        });
+        ButterKnife.bind(this);
+        initView();
     }
 
-    protected void makeRequest() {
+    private void initView(){
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA);
+        int permissionLocation = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            makeRequestCamera();
+        }
+
+        if (permissionLocation != PackageManager.PERMISSION_GRANTED) {
+            makeRequestLocation();
+        }
+    }
+
+    @OnClick(R.id.b_qr_code)
+    public void scanQRCode(){
+        Intent intent = new Intent(MainActivity.this, ReadQR.class);
+        startActivityForResult(intent, 1);
+    }
+
+    @OnClick(R.id.iv_map)
+    public void map(){
+        startActivity(new Intent(MainActivity.this, MapsActivity.class));
+    }
+
+    protected void makeRequestCamera() {
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.CAMERA},
+                RECORD_REQUEST_CODE);
+    }
+
+    protected void makeRequestLocation(){
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                 RECORD_REQUEST_CODE);
     }
 
@@ -63,11 +88,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         String getValidation;
         super.onActivityResult(requestCode, resultCode, data);
-        getValidation = data.getStringExtra(ConstantPreferences.KEY_VALIDATION);
         if (requestCode == 1){
             if (resultCode == RESULT_OK){
+                getValidation = data.getStringExtra(ConstantPreferences.KEY_VALIDATION);
                 if (getValidation.equals(ConstantPreferences.KEY_GET_VALIDATION)){
-                    bAbsence.setText("Check Out");
+                    bAbsence.setText(ConstantPreferences.CHECK_OUT);
                 }
             }
         }
--- app/src/main/java/com/otret/absence/MapsActivity.java
+++ app/src/main/java/com/otret/absence/MapsActivity.java
@@ -1,18 +1,46 @@
 package com.otret.absence;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Double longitude;
+    private Double latitude;
+    Double officeLatitude = -7.7197619;
+    Double officeLongitude = 110.3825028;
+    float distance;
+    private List<Marker> markers = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +50,83 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        FusedLocationProviderClient mFusedLocation = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        mFusedLocation.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                    longitude = location.getLongitude();
+                    latitude = location.getLatitude();
+                    LatLng you = new LatLng(latitude,longitude);
+                    markers.add(mMap.addMarker(new MarkerOptions().position(you)
+                            .title(ConstantPreferences.YOUR_MARKER)
+                            .icon(bitmapDescriptorFromVector(MapsActivity.this, R.drawable.ic_person_pin_circle_blue))));
+//                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(you, 19.0f));
+
+                    officeMark();
+                    cameraMarker(mMap);
+                    distanceBetween(latitude, officeLatitude, longitude, officeLongitude);
+
+                    if (distance<50){
+                        ConstantPreferences.toastMessage(MapsActivity.this, ConstantPreferences.YOUR_MARKER);
+                    } else {
+                        ConstantPreferences.toastMessage(MapsActivity.this, ConstantPreferences.MARKER_OFFICE);
+                    }
+            }
+        });
+    }
+
+    private void cameraMarker(GoogleMap googleMap){
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        for (Marker marker : markers) {
+            builder.include(marker.getPosition());
+        }
+        LatLngBounds bounds = builder.build();
+        int padding = 100; // offset from edges of the map in pixels
+
+        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+        googleMap.animateCamera(cu);
+    }
+
+    private void officeMark(){
+        int radius = 50;
+        LatLng office = new LatLng(officeLatitude,officeLongitude);
+        mMap.addCircle(new CircleOptions()
+                .center(office)
+                .radius(radius)
+                .strokeColor(Color.RED)
+                .fillColor(Color.TRANSPARENT));
+        markers.add(mMap.addMarker(new MarkerOptions().position(office).title(ConstantPreferences.MARKER_OFFICE)));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(office, 19.0f));
+    }
+
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+
+    private void distanceBetween(Double latitude1, Double latitude2, Double longitude1, Double longitude2){
+        Location loc1 = new Location("");
+        loc1.setLatitude(latitude1);
+        loc1.setLongitude(longitude1);
+        Location loc2 = new Location("");
+        loc2.setLatitude(latitude2);
+        loc2.setLongitude(longitude2);
+        distance = loc1.distanceTo(loc2);
     }
 }
